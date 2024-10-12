0d5d7a01391a1173d432540f7ec2f4af555a9f39
bartosz rzepa <bartosz.rzepa@swmansion.com>
2024-10-12 20:09:56 +0200

    view, no logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import './App.css';
 // import RoutingTable from './RoutingTable';
 import Homepage from './Homepage';
 
 function App() {
+    const customTheme = extendTheme({
+        components: {
+            Text: {
+                baseStyle: {
+                    fontWeight: 'normal',
+                    color: 'gray.900'
+                }
+            },
+        },
+    });
+
   return (
-    <div className="App">
+    <ChakraProvider theme={customTheme}>
         <Homepage />
-    </div>
+    </ChakraProvider>
   );
 }
 
