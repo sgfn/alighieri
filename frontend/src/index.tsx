de0cb171db796d63b6c321468bc774873ed9c54a
bartosz rzepa <bartosz.rzepa@swmansion.com>
2024-10-12 20:13:02 +0200

    default text color

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -10,9 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-        <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
 
