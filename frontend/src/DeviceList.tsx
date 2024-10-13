480afa42a2f6fc98d68f1268e16b30d6448f7c8e
bartosz rzepa <bartosz.rzepa@swmansion.com>
2024-10-13 13:20:18 +0200

    device details view

--- frontend/src/DeviceList.tsx
+++ frontend/src/DeviceList.tsx
@@ -1,24 +1,27 @@
 import { CheckCircleIcon, QuestionIcon, WarningIcon } from "@chakra-ui/icons";
-import { Button, Flex, Spacer, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Flex, Spacer, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import DeviceDetails from "./DeviceDetails";
 
-type Device = {
+export type Device = {
     name: string,
     io: string,
     ip: string,
+    mac: string,
+    sampleRate: string,
     status: DeviceStatus
 }
 
 export default function DeviceList() {
     const devices = [
-        { name: "DA44AU", io: "4/4", ip: "10.0.0.1", status: DeviceStatus.Ok },
-        { name: "DA11AEN", io: "1/0", ip: "10.0.0.2", status: DeviceStatus.Error },
-        { name: "DA11USB", io: "2/2", ip: "10.0.0.3", status: DeviceStatus.Ok },
-        { name: "ETS630DT", io: "0/1", ip: "10.0.0.4", status: DeviceStatus.Unknown }
+        { name: "DA44AU", io: "4/4", ip: "10.0.0.1", mac:"FF:FF:FF:FF:FF:FF", sampleRate:"192KHz", status: DeviceStatus.Ok },
+        { name: "DA11AEN", io: "1/0", ip: "10.0.0.2", mac:"FF:FF:FF:FF:FF:FF", sampleRate:"192KHz", status: DeviceStatus.Error },
+        { name: "DA11USB", io: "2/2", ip: "10.0.0.3", mac:"FF:FF:FF:FF:FF:FF", sampleRate:"192KHz", status: DeviceStatus.Ok },
+        { name: "ETS630DT", io: "0/1", ip: "10.0.0.4", mac:"FF:FF:FF:FF:FF:FF", sampleRate:"192KHz", status: DeviceStatus.Unknown },
     ];
 
     return (
         <TableContainer>
-            <Table variant='simple'>
+            <Table variant='simple' size='sm'>
                 <Thead>
                     <Tr>
                         <Th color='gray.900'>Name</Th>
@@ -48,20 +51,21 @@ function DeviceRow(device: Device) {
                 <Flex alignItems='center'>
                     <Text>{device.ip}</Text>
                     <Spacer/>
-                    <Button variant='outline' borderWidth='2px' borderColor='gray.600' height='30px'>Details</Button>
+                    {/* <Button variant='outline' borderWidth='2px' borderColor='gray.600' height='30px'>Details</Button> */}
+                    <DeviceDetails {...device}/>
                 </Flex>
             </Td>
         </Tr>
     )
 }
 
-enum DeviceStatus {
+export enum DeviceStatus {
     Ok = "OK",
     Unknown = "UNKNOWN",
     Error = "Error"
 }
 
-function StatusToIcon(status: DeviceStatus) {
+export function StatusToIcon(status: DeviceStatus) {
     if (status === DeviceStatus.Ok) {
         return(<CheckCircleIcon color='green.400' />)
     } else if (status === DeviceStatus.Error) {
