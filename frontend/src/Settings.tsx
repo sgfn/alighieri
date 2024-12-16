a5d35c7c2548e5ebb4df68cc9cad1194b6c9ff0e
bartosz rzepa <bartosz.rzepa@swmansion.com>
2024-12-16 09:11:39 +0100

    adapt layout to api

--- frontend/src/Settings.tsx
+++ frontend/src/Settings.tsx
@@ -1,12 +1,19 @@
-import { Box, Flex, Input, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Text, VStack } from "@chakra-ui/react";
 
 
 export default function Settings() {
     return (
         <Box>
             <Text fontWeight='semibold'>IP address range configuration</Text>
-            <InputField text={'IP address'} input_placeholder={'255.255.255.255'} />
-            <InputField text={'netmask'} input_placeholder={'24'} />
+            <InputField text={'from'} input_placeholder={'255.255.255.255'} />
+            <InputField text={'to'} input_placeholder={'255.255.255.255'} />
+            <InputField text={'netmask'} input_placeholder={'255.255.255.255'} />
+            <Button ml='10px' bg='gray.300' color='gray.900' _hover={{ bg: 'gray.400', color: 'gray.800' }}>update</Button>
+            <VStack align='start' mt='24px' pt='16px' borderTop='1px solid' borderColor='gray.300'>
+                <Text fontWeight='semibold'>system configuration</Text>
+                <Button w='192px' bg='gray.600' color='gray.50' _hover={{ bg: 'gray.500', color: 'gray.100' }}>import from file</Button>
+                <Button w='192px' bg='gray.300' color='gray.900' _hover={{ bg: 'gray.400', color: 'gray.800' }}>export to file</Button>
+            </VStack>
         </Box>
     )
 }
