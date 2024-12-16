083a48ff6c718b84f791542300009d3b80834e2b
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2024-12-16 09:22:27 +0100

    woirqoirjorj

--- controller/lib/alighieri_controller/dhcp.ex
+++ controller/lib/alighieri_controller/dhcp.ex
@@ -18,6 +18,8 @@ defmodule Alighieri.Controller.DHCP do
   @impl true
   def init(config) do
     {iface, config} = Keyword.pop(config, :iface, @default_iface)
+    config = prep_config(config)
+
     Logger.debug("Starting DHCP server\nIface: #{inspect(iface)}\nConfig: #{inspect(config)}")
     {:ok, server} = DHCPServer.start_link(iface, config)
 
@@ -26,10 +28,26 @@ defmodule Alighieri.Controller.DHCP do
 
   @impl true
   def handle_call({:apply_config, config}, _from, state) do
+    config = prep_config(config)
     Logger.debug("Restarting DHCP server\nConfig: #{inspect(config)}")
     :ok = DHCPServer.stop(state.server)
     {:ok, server} = DHCPServer.start_link(state.iface, config)
 
     {:reply, :ok, %{state | server: server}}
+  end
+
+  defp prep_config(config) do
+    {faux_gateway, range_to} = Keyword.fetch!(config, :range)
+
+    [a, b, c, d] = String.split(faux_gateway, ".")
+    d = d |> String.to_integer() |> then(&(&1 + 1)) |> to_string()
+    range_from = Enum.join([a, b, c, d], ".")
+
+    [
+      netmask: config[:netmask],
+      range: {range_from, range_to},
+      gateway: faux_gateway,
+      domain_servers: []
+    ]
   end
 end
